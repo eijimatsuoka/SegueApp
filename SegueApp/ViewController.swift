7d1bd99465f2a24629b44fe2342f22e4089fbed9
eijimatsuoka <t.jgreeeen550229@icloud.com>
2021-11-29 00:57:15 +0900

    Initial Commit

---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by 松岡栄志 on 2021/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
